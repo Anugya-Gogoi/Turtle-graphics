f04173a8d4d5a5c2cd51ab6562d0839e3e8c199a
Anugya Gogoi <55389818+Anugya-Gogoi@users.noreply.github.com>
2020-10-20 23:58:57 +0530

    Add files via upload

---
+++ redd.py
@@ -0,0 +1,32 @@
+import turtle
+from random import randint
+draw = turtle.Turtle()
+
+draw.speed(10)
+
+turtle.bgcolor("black")
+
+
+for i in range(185):
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+
+    turtle.colormode(255)
+    draw.pencolor(r, g, b)
+    draw.backward(90)
+    draw.right(30)
+    draw.backward(20)
+    draw.left(60)
+    draw.forward(80)
+    draw.right(30)
+
+    draw.penup()
+    draw.setposition(0, 0)
+    draw.pendown()
+
+    draw.right(2)
+
+
+
+turtle.done()
